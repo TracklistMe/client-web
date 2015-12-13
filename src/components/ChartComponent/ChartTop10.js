30229c142682cb12ebc5e3426dff3f4e4e9632f9
Nicola Bortignon <info@nicolabortignon.com>
2015-12-13 09:35:19 -0800

    Chart's header

--- src/components/ChartComponent/ChartTop10.js
+++ src/components/ChartComponent/ChartTop10.js
@@ -1,11 +1,18 @@
 import React, {Component} from 'react';
+import CustomButton from '../Buttons/CustomButton';
 
 export default class ChartTop10 extends Component {
   render() {
     return (
       <div>
       	<div className="chart">
-      		asd
+          <div className="chartHeader">
+            <div className="chartHeadline">
+              <div className="title">TOP 10</div>
+              <div className="subtitle"> Most download tracks</div>
+            </div>
+            <CustomButton name="Play All" icon />
+          </div>
       	</div>
       	<div className="bottomLine">
       	</div>
