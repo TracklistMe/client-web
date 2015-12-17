b3a69913be81f4b278af733bf40fd1cc5dcbb80e
Nicola Bortignon <info@nicolabortignon.com>
2015-12-17 14:09:07 -0800

    Adding date recap component in tracklist

--- src/components/Jumbotron/TracklistJumbotron.js
+++ src/components/Jumbotron/TracklistJumbotron.js
@@ -26,22 +26,27 @@ export default class TracklistJumbotron extends Component {
         <MainHeaderBackground image="http://www.global-sets.com/wp-content/uploads/2014/05/adam-beyer-live-660x330.jpg" />
         <div className="headerContent">
           <div className="row trackJumbotronContainer">
-            <div className="hidden-xs hidden-sm col-sub-xs-5 col-sub-sm-6 col-sub-md-6 col-sub-lg-5 overflowHidden">
+            <div className="hidden-xs hidden-sm col-sub-xs-5 col-sub-sm-6 col-sub-md-6 col-sub-lg-4 overflowHidden">
               <img className="cover" src="http://www.sorstu.ca/wp-content/uploads/artistes/square/adam-beyer.jpg" />
             </div>
-            <div className="col-sub-xs-18 col-sub-sm-18 col-sub-md-12 col-sub-lg-13">
+            <div className="col-sub-xs-18 col-sub-sm-18 col-sub-md-12 col-sub-lg-14">
               <div className="row">
                 <div className="col-lg-6 text-left trackDescriptionSpace">
-                  <div className="titleContainer">
-                      <h1>Adam Beyer</h1>
-                      <h4>Stockholm, Sweden</h4>
-                  </div>
-                  <div className="dateHolder">
-                   asd
-                  </div>
+                  <h1>Adam Beyer</h1>
+                  <h4>Stockholm, Sweden</h4>
                 </div>
                 <div className="col-lg-6 text-right">
                   sharing Stats
+                </div>
+              </div>
+              <div className="row">
+                <div className="col-lg-12">
+                  <div className="dateHolder">
+                    <div className="dateNumber">18</div>
+                    <div className="dateMonth">Mar<br />2016</div>
+                    <div className="verticalDivider"></div>
+                    <div className="eventName"> Ultra Music Festival </div>
+                  </div>
                 </div>
               </div>
             </div>
