c30cc8c1ba3fd06da1fed6bcda3d6464446e4bcb
Nicola Bortignon <info@nicolabortignon.com>
2015-12-29 16:25:13 -0800

    Adding react-dropzone library and remove react-dropzone-component.

--- src/components/Jumbotron/ArtistJumbotron.js
+++ src/components/Jumbotron/ArtistJumbotron.js
@@ -1,99 +1,46 @@
 import React, {Component, PropTypes} from 'react';
 import ChartTop10 from '../ChartComponent/ChartTop10';
 import MainHeaderBackground from '../MainHeader/MainHeaderBackground';
-import DropzoneComponent from 'react-dropzone-component';
 import {apiEndPoint} from '../../helpers/ApiClient';
-import ReactDOMServer from 'react-dom/server';
+import Dropzone from 'react-dropzone';
+
+
 export default class ArtistJumbotron extends Component {
   static propTypes = {
     artist: PropTypes.shape({
       avatar: PropTypes.string.isRequired
-    })
+    }),
+    onDrop: PropTypes.func
   }
 
+  constructor(props) {
+    super(props);
+    this.onDrop = this.onDrop.bind(this);
+  }
+
+  state = {
+    files: ['asd']
+  };
+
+  onDrop(files) {
+    console.log('DROPPED A FILE BUM! ');
+    console.log(files);
+    this.setState({
+      files: files
+    });
+    console.log(this.state);
+  }
+
+
   render() {
-    const djsConfig = {
-      addRemoveLinks: true,
-      params: {
-        myParameter: "I'm a parameter!"
-      },
-      previewTemplate: ReactDOMServer.renderToStaticMarkup(
-        <div className="dz-preview dz-file-preview">
-          <div className="dz-details">
-            <div className="dz-filename"><span data-dz-name></span></div>
-            <img data-dz-thumbnail />
-          </div>
-          <div className="dz-progress"><span className="dz-upload" data-dz-uploadprogress></span></div>
-          <div className="dz-success-mark"><span>✔</span></div>
-          <div className="dz-error-mark"><span>✘</span></div>
-          <div className="dz-error-message">This is an error: <span data-dz-errormessage></span></div>
-        </div>
-      )
-    };
-
-    const callbackArray = [
-      function firstCallBack() {
-        console.log('Look Ma, I\'m a callback in an array!');
-      },
-      function secondCallBack() {
-        console.log('Wooooow!');
-      }
-    ];
-
-    const simpleCallBack = function callback() {
-      console.log('I\'m a simple callback');
-    };
-
-    const eventHandlers = {
-      // This one receives the dropzone object as the first parameter
-      // and can be used to additional work with the dropzone.js
-      // object
-      init: null,
-      // All of these receive the event as first parameter:
-      drop: callbackArray,
-      dragstart: null,
-      dragend: null,
-      dragenter: null,
-      dragover: null,
-      dragleave: null,
-      // All of these receive the file as first parameter:
-      addedfile: simpleCallBack,
-      removedfile: null,
-      thumbnail: null,
-      error: null,
-      processing: null,
-      uploadprogress: null,
-      sending: null,
-      success: null,
-      complete: null,
-      canceled: null,
-      maxfilesreached: null,
-      maxfilesexceeded: null,
-      // All of these receive a list of files as first parameter
-      // and are only called if the uploadMultiple option
-      // in djsConfig is true:
-      processingmultiple: null,
-      sendingmultiple: null,
-      successmultiple: null,
-      completemultiple: null,
-      canceledmultiple: null,
-      // Special Events
-      totaluploadprogress: null,
-      reset: null,
-      queuecompleted: null
-    };
-    const uploadAPIendpoint = apiEndPoint() + '/artists/58/profilePicture/100/100';
-    const componentConfig = {
-      postUrl: uploadAPIendpoint,
-    };
-
+    console.log(this.props);
     const {artist} = this.props; // eslint-disable-line no-shadow
     if (!artist) {
       return (<div>Loading Artist Profile</div>);
     }
     return (
       <div className="artistJumbotron">
-        <MainHeaderBackground image={apiEndPoint + '/images/' + artist.avatar} />
+        <MainHeaderBackground image={apiEndPoint() + '/images/' + artist.avatar} />
         <div className="headerContent">
           <div className="row artistJumbotronContainer">
             <div className="hidden-xs hidden-sm col-sub-xs-5 col-sub-sm-6 col-sub-md-6 col-sub-lg-5 overflowHidden">
@@ -102,9 +49,17 @@ export default class ArtistJumbotron extends Component {
             <div className="col-sub-xs-9 col-sub-sm-9 col-sub-md-6 col-sub-lg-7">
               <div className="row">
                 <div className="col-lg-12 text-left trackDescriptionSpace">
-                  <DropzoneComponent config={componentConfig} eventHandlers={eventHandlers} djsConfig={djsConfig} />
+                  <div>
+                    <Dropzone ref="dropzone" id="dropzone" onDrop={this.onDrop}>
+                        <div>Try dropping some files here, or click to select files to upload.</div>
+                    </Dropzone>
+                    {this.state.files.length > 0 ? <div>
+                    <h2>Uploading {this.state.files.length} files...</h2>
+                    <div>{this.state.files.map((file) => <img src={file.preview} /> )}</div>
+                    </div> : null}
+                  </div>
                   <h1>{artist.displayName}</h1>
-                  <h4>Rome, Italy</h4>
+                  <h4>{this.state.files.length} Rome, Italy</h4>
                 </div>
               </div>
               <div className="row inlineBlock">
