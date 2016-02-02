14b7ba828f7724ec07bcddc60e7e35f10760537f
Nicola Bortignon <info@nicolabortignon.com>
2016-02-02 08:19:18 -0800

    adding new duck

--- src/redux/modules/player.js
+++ src/redux/modules/player.js
@@ -1,14 +1,25 @@
 import * as types from '../../constants/ActionTypes';
+// Load cart informations
+const PLAYER_ADD_TRACK = 'PLAYER_ADD_TRACK';
+const PLAYER_ADD_TRACK_SUCCESS = 'PLAYER_ADD_TRACK_SUCCESS';
+const PLAYER_ADD_TRACK_FAILURE = 'PLAYER_ADD_TRACK_FAILURE';
 
 const initialState = {
   currentSongIndex: null,
   currentTime: 0,
   isPlaying: false,
-  selectedPlaylists: []
+  selectedPlaylists: [],
+  playlist: []
 };
 
 export default function player(state = initialState, action) {
   switch (action.type) {
+    case PLAYER_ADD_TRACK:
+      break;
+    case PLAYER_ADD_TRACK_SUCCESS:
+      break;
+    case PLAYER_ADD_TRACK_FAILURE:
+      break;
     case types.CHANGE_CURRENT_TIME:
       console.log(action.time);
       return {
@@ -19,21 +30,26 @@ export default function player(state = initialState, action) {
       return Object.assign({}, state, {
         currentSongIndex: action.songIndex
       });
-
     case types.CHANGE_SELECTED_PLAYLISTS:
       return Object.assign({}, state, {
         selectedPlaylists: action.playlists
       });
-
     case types.RESET_AUTHED:
       return Object.assign({}, state, initialState);
-
     case types.TOGGLE_IS_PLAYING:
       return Object.assign({}, state, {
         isPlaying: action.isPlaying
       });
-
     default:
       return state;
   }
+}
+
+export function addTrack(song, startingTime, waitInQueue) {
+  return {
+    types: [PLAYER_ADD_TRACK, PLAYER_ADD_TRACK_SUCCESS, PLAYER_ADD_TRACK_FAILURE],
+    song: song,
+    startingTime: startingTime,
+    waitInQueue: waitInQueue
+  };
 }
