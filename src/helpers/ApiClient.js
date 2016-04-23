8e7d644af542f69162151ada09b06348d3635d35
Nicola Bortignon <info@nicolabortignon.com>
2016-04-22 18:38:47 -0700

    fixing bug with babel and API CLIENT

--- src/helpers/ApiClient.js
+++ src/helpers/ApiClient.js
@@ -25,7 +25,7 @@ function formatUrl(path) {
  *
  * Remove it at your own risk.
  */
-class _ApiClient {
+export default class ApiClient {
   constructor(req) {
     methods.forEach((method) =>
       this[method] = (path, { params, data } = {}) => new Promise((resolve, reject) => {
@@ -46,9 +46,18 @@ class _ApiClient {
         request.end((err, { body } = {}) => err ? reject(body || err) : resolve(body));
       }));
   }
+   /*
+   * There's a V8 bug where, when using Babel, exporting classes with only
+   * constructors sometimes fails. Until it's patched, this is a solution to
+   * "ApiClient is not defined" from issue #14.
+   * https://github.com/erikras/react-redux-universal-hot-example/issues/14
+   *
+   * Relevant Babel bug (but they claim it's V8): https://phabricator.babeljs.io/T2455
+   *
+   * Remove it at your own risk.
+   */
+  empty() {}
 }
-
-const ApiClient = _ApiClient;
 
 export function apiEndPoint() {
   if (__DEVELOPMENT__) {
@@ -57,5 +66,3 @@ export function apiEndPoint() {
   // Prepend `/api` to relative URL, to proxy to API server.
   return PROD_API_ENDPOINT;
 }
-
-export default ApiClient;
